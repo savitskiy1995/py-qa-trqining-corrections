a160643649f9d3ee69b2be14e4aa44feea2c5057
savitskiy1995 <glebsav@bk.ru>
2025-05-16 16:45:38 +0300

    21,22,23
    Поменял тесты и добавил новый check_all_contacts

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -8,7 +8,7 @@ db = ORMfixture(host='127.0.0.1', name='addressbook', user='root', password='')
 
 def test_add_contact_to_group(app):
     if len(db.get_contact_list()) == 0:
-        app.contact.create(Contact(firstname="Name", lastname="Lastname"))
+        app.contact.create_contact(Contact(firstname="Name", lastname="Lastname"))
     if len(db.get_group_list()) == 0:
         app.group.create(Group(name="Test"))
     old_contacts = db.get_contact_list()
@@ -17,7 +17,7 @@ def test_add_contact_to_group(app):
     group_from_old = random.choice(old_groups)
     old_contacts_in_group = db.get_contacts_in_group(group_from_old)
     if len(db.get_contact_list()) == len(old_contacts_in_group):
-            app.contact.create(Contact(firstname="Michael", lastname="Jordan"))
+            app.contact.create_contact(Contact(firstname="Michael", lastname="Jordan"))
     app.contact.add_contact_to_group(contact_from_old, group_from_old)
     new_contacts_in_group = db.get_contacts_in_group(group_from_old)
     assert len(old_contacts_in_group) + 1 == len(new_contacts_in_group)
